6c4e92ed51df5349c3468594e99c68d5b16fe508
Thai-max948 <len74282@gmail.com>
2026-09-13 10:40:23 +0700

    LibraryManagement Update!

--- LibraryManagement/obj/Debug/net10.0-windows/Views/Books/BooksView.g.i.cs
+++ LibraryManagement/obj/Debug/net10.0-windows/Views/Books/BooksView.g.i.cs
@@ -53,7 +53,7 @@ namespace LibraryManagement.Views.Books {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LibraryManagement;component/views/books/booksview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LibraryManagement;V1.0.0.0;component/views/books/booksview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Books\BooksView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
